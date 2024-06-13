a7c133e1fa817bdae0adef9e80fb355beaa5e7e1
JAIGURU <136327019+jai2992@users.noreply.github.com>
2024-06-13 19:45:47 +0530

    Update sece_chat_load.py

--- sece_chat_load.py
+++ sece_chat_load.py
@@ -6,7 +6,7 @@ from pinecone import Pinecone
 from langchain_pinecone import PineconeVectorStore
 import os
 
-os.environ["PINECONE_API_KEY"] = 'a192f216-4cd3-4b0c-b5b7-1db783efd833'
+os.environ["PINECONE_API_KEY"] = 'your api key'
 
 def doc_load(documents):
     file_loader=PyPDFDirectoryLoader(documents)
@@ -20,5 +20,5 @@ def chunk_split(docs):
     return doc
 documents=chunk_split(doc)
 
-embeddings = HuggingFaceInferenceAPIEmbeddings(api_key='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', model_name="sentence-transformers/all-MiniLM-l6-v2")
-PineconeVectorStore.from_documents(documents, embeddings, index_name='sece-chat')
+embeddings = HuggingFaceInferenceAPIEmbeddings(api_key='your api key', model_name="sentence-transformers/all-MiniLM-l6-v2")
+PineconeVectorStore.from_documents(documents, embeddings, index_name='your index name')
